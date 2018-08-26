dc183309543d751b007c963035f4746772020bae
Panagiotis Roubatsis <proubatsis@gmail.com>
2018-08-26 14:54:20 -0400

    Remove hardcoded values

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,7 @@ object Main {
     //    transmitter.transmit(List(0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21), secondsPerByte = 0.1f, step = 1000, low = 1000)
 
     val receiver = new Receiver with JavaSoundRecorder with JTransformSpectrumTransformer with SamplingRate
-    val ctx = BucketContext()
+    val ctx = BucketContext(startingValue = 3000)
 
     receiver.start()
 
@@ -23,7 +23,7 @@ object Main {
     } sliding 2 collect { case Seq(a, b) if a != b => b } toList
 
 
-    val decoded = FrequencyDecoder.decode(recording, FrequencyBucket.createWithAbsVariation(1000, 50), FrequencyBucket.createWithAbsVariation(15000, 50), ctx.freqToNibble)
+    val decoded = FrequencyDecoder.decode(recording, FrequencyBucket.createWithAbsVariation(ctx.startingValue, ctx.variation), FrequencyBucket.createWithAbsVariation(ctx.endValue, ctx.variation), ctx.freqToNibble)
     println(new String(decoded.toArray, StandardCharsets.UTF_8))
   }
 }
